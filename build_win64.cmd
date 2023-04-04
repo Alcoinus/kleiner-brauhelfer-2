cfa172ce21daf6ac0d42db2107d93a33d4ccdcaa
Felix Theiß <felix.theiss@outlook.com>
2023-04-04 19:21:11 +0200

    Use default Qt location and allow usage of Build Tools

--- build_win64.cmd
+++ build_win64.cmd
@@ -1,14 +1,19 @@
 @echo off
 
-set QT_DIR=%1
+if not "%~1" == "" (
+  set QT_DIR=%1
+) else (
+  set QT_DIR="C:\Qt\6.5.0\msvc2019_64\bin"
+)
 set QT_DIR=%QT_DIR:"=%
 
 set PRO=%~dp0kleiner-brauhelfer-2.pro
 set BUILD_DIR=%~dp0build-win64
 
-if "%QT_DIR%" equ "" (
+if not exist "%QT_DIR%" (
+  echo Error: Qt directory not found
   echo Usage: build_win64.cmd ^<qt-bin-directory^>
-  echo Example: build_win64.cmd C:\Qt\6.5.0\msvc2019_64\bin
+  echo Default: build_win64.cmd C:\Qt\6.5.0\msvc2019_64\bin
   exit /b -1
 )
 
@@ -17,7 +22,11 @@ if not exist "%QT_DIR%\qmake.exe" (
   exit /b -1
 )
 
-for /f "usebackq delims=#" %%a in (`"%PROGRAMFILES(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set VSPATH=%%a
+for /f "usebackq delims=#" %%a in (`"%PROGRAMFILES(x86)%\Microsoft Visual Studio\Installer\vswhere" -products * -latest -property installationPath`) do set VSPATH=%%a
+if not defined VSPATH (
+  echo Error: Visual Studio installation not found
+  exit \b -1
+)
 call "%VSPATH%\VC\Auxiliary\Build\vcvars64.bat" > nul 2>&1
 
 mkdir %BUILD_DIR%
